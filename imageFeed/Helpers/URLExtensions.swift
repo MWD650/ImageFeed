e0fb0cba657688b87f5c325741ae88d115d8b7b9
Alexandr Polanskiy <mwd650@yandex.ru>
2023-06-12 16:25:49 +0500

    minor fix

--- imageFeed/Helpers/URLExtensions.swift
+++ imageFeed/Helpers/URLExtensions.swift
@@ -28,35 +28,6 @@ extension URLRequest {
         }
 }
 
-//extension URLSession {
-//    func data(for request: URLRequest, completion: @escaping (Result<Data, Error>) -> Void) -> URLSessionTask {
-//
-//        let fulfillCompletion: (Result<Data, Error>) -> Void = { result in
-//            DispatchQueue.main.async {
-//                completion(result)
-//            }
-//        }
-//
-//        let task = dataTask(with: request) { data, responce, error in
-//            if let data = data,
-//               let responce = responce,
-//               let statusCode = (responce as? HTTPURLResponse)?.statusCode {
-//                if 200..<300 ~= statusCode {
-//                    fulfillCompletion(.success(data))
-//                } else {
-//                    fulfillCompletion(.failure(NetworkError.httpStatusCode(statusCode)))
-//                }
-//            } else if let error = error {
-//                fulfillCompletion(.failure(NetworkError.urlRequestError(error)))
-//            } else {
-//                fulfillCompletion(.failure(NetworkError.urlSessionError))
-//            }
-//        }
-//        task.resume()
-//        return task
-//    }
-//}
-
 extension URLSession {
     func objectTask<T: Decodable>(
         for request: URLRequest,
